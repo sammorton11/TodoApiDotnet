97df51df9936d1762054b79ddda786baf9b214cb
sammorton11 <sammorton11@yahoo.com>
2023-07-28 15:21:32 -0500

    Fixing memory management issues

--- WebApi01/Controllers/TodoAppController.cs
+++ WebApi01/Controllers/TodoAppController.cs
@@ -2,160 +2,230 @@
 using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApi01.Controllers
+namespace WebApi01.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TodoAppController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class TodoAppController : ControllerBase
+    private readonly string _sqlDataSource;
+    private SqlDataReader? _reader;
+    private readonly DataTable? _table;
+    private readonly SqlConnection? _sqlConnection;
+    public TodoAppController(IConfiguration config) 
     {
-        private readonly string _sqlDataSource;
-        public TodoAppController(IConfiguration config) 
+        _sqlDataSource = config.GetConnectionString("todoAppDBConnectionString");
+        _sqlConnection = GetConnection(_sqlDataSource);
+        _table = new DataTable();
+    }
+
+    private static SqlDataReader GetReader(SqlCommand command, IDbConnection? connection)
+    {
+        connection?.Open();
+        return command.ExecuteReader(CommandBehavior.CloseConnection);
+    }
+
+    private static SqlConnection GetConnection(string source)
+    {
+        SqlConnection connection = new(source);
+        return connection;
+    }
+
+
+    private static SqlCommand GetCommand(string query, SqlConnection? connection)
+    {
+        return new SqlCommand(query, connection);
+    }
+
+
+    [HttpGet]
+    [Route("GetNotes")]
+    public JsonResult GetNotes() 
+    {
+        const string query = "SELECT * FROM dbo.notes";
+
+        using (_sqlConnection) 
         {
-            _sqlDataSource = config.GetConnectionString("todoAppDBConnectionString");
-        }
-        
-
-
-        [HttpGet]
-        [Route("GetNotes")]
-        public JsonResult GetNotes() 
-        {
-            const string query = "SELECT * FROM dbo.notes";
-            var table = new DataTable();
-
-            using (SqlConnection myCon = new(_sqlDataSource)) 
+            try
             {
-                myCon.Open();
-                using SqlCommand myCommand = new(query, myCon);
-                var myReader = myCommand.ExecuteReader();
-                table.Load(myReader);
-        
-                myReader.Close();
-                myCon.Close();
+                using var myCommand = GetCommand(query, _sqlConnection);
+                if (_sqlConnection != null) _reader = GetReader(myCommand, _sqlConnection);
+                if (_reader != null) _table?.Load(_reader);
             }
-        
-            return new JsonResult(table);
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                _sqlConnection?.Close();
+            }
         }
 
-        
-        
-
-        [HttpGet]
-        [Route("GetNote")]
-        public JsonResult GetNote(int id) 
+        using (_table)
         {
-            const string query = "SELECT * FROM dbo.notes where NoteId=@id";
-            var table = new DataTable();
+            return new JsonResult(_table);
+        }
+    }
 
-            using (SqlConnection myCon = new(_sqlDataSource)) 
+        
+        
+
+    [HttpGet]
+    [Route("GetNote")]
+    public JsonResult GetNote(int id) 
+    {
+        const string query = "SELECT * FROM dbo.notes where NoteId=@id";
+            
+        using (_sqlConnection) 
+        {
+            try
             {
-                myCon.Open();
-                using (SqlCommand myCommand = new(query, myCon))
-                {
-                    myCommand.Parameters.AddWithValue("@id", id);
-                    var myReader = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
-                    table.Load(myReader);
+                using var myCommand = GetCommand(query, _sqlConnection);
+                myCommand.Parameters.AddWithValue("@id", id);
                     
-                    myReader.Close();
-                    myCon.Close();
-                }
+                if (_sqlConnection != null) _reader = GetReader(myCommand, _sqlConnection);
+                if (_reader != null) _table?.Load(_reader);
             }
-
-            return new JsonResult(table);
-        }
-        
-        
-
-        [HttpPost]
-        [Route("AddNotes")]
-        public JsonResult AddNotes([FromForm] string title, [FromForm] string description) 
-        {
-            const string query = "INSERT into dbo.notes (Title, Description) values(@title, @description)";
-            DataTable table = new();
-
-            using (SqlConnection myCon = new(_sqlDataSource)) 
+            catch (Exception e)
             {
-                myCon.Open();
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                _sqlConnection?.Close();
+            }
+        }
 
-                using SqlCommand myCommand = new(query, myCon);
+        using (_table)
+        {
+            return new JsonResult(_table);
+        }
+    }
+        
+        
+
+    [HttpPost]
+    [Route("AddNotes")]
+    public JsonResult AddNotes([FromForm] string title, [FromForm] string description) 
+    {
+        const string query = "INSERT into dbo.notes (Title, Description) values(@title, @description)";
+
+        using (_sqlConnection) 
+        {
+            try
+            {
+                using var myCommand = GetCommand(query, _sqlConnection);
                 myCommand.Parameters.AddWithValue("@title", title);
                 myCommand.Parameters.AddWithValue("@description", description);
-                var myReader = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
-                table.Load(myReader);
-
-                myReader.Close();
-                myCon.Close();
+                    
+                if (_sqlConnection != null) _reader = GetReader(myCommand, _sqlConnection);
+                if (_reader != null) _table?.Load(_reader);
             }
-
-            return new JsonResult("Added Successfully");
-        }
-        
-        
-
-        [HttpDelete]
-        [Route("DeleteNotes")]
-        public JsonResult DeleteNotes(int id)
-        {
-            const string query = "DELETE from dbo.notes where NoteId=@id";
-            DataTable table = new();
-
-            using (SqlConnection myCon = new(_sqlDataSource))
+            catch (Exception e)
             {
-                myCon.Open();
-                using (SqlCommand myCommand = new(query, myCon))
-                {
-                    myCommand.Parameters.AddWithValue("@id", id);
-                    var myReader = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
-                    table.Load(myReader);
-                    myReader.Close();
-                    myCon.Close();
-                }
+                Console.WriteLine(e);
+                throw;
             }
-
-            return new JsonResult("Deleted Successfully");
-        }
-        
-        
-        
-        [HttpDelete]
-        [Route("DeleteAllNotes")]
-        public JsonResult DeleteAllNotes()
-        {
-            const string query = "DELETE from dbo.notes";
-
-            using (SqlConnection myCon = new(_sqlDataSource))
+            finally
             {
-                myCon.Open();
-                using (SqlCommand myCommand = new(query, myCon))
-                {
-                    myCommand.ExecuteNonQuery(); // Use ExecuteNonQuery for DELETE
-                }
+                _sqlConnection?.Close();
             }
-
-            return new JsonResult("Deleted Successfully");
         }
-        
-        
 
-        [HttpPut]
-        [Route("UpdateNote")]
-        public JsonResult UpdateNote([FromForm] int id, [FromForm] string title, [FromForm] string description)
+        using (_table)
         {
-            const string query = "UPDATE dbo.notes SET Title = @title, Description = @description WHERE NoteId = @id";
-            var sqlDataSource = this._sqlDataSource;
+            return new JsonResult(_table);
+        }
+    }
+        
+        
 
-            using var myCon = new SqlConnection(sqlDataSource);
-            myCon.Open();
+    [HttpDelete]
+    [Route("DeleteNotes")]
+    public JsonResult DeleteNotes(int id)
+    {
+        const string query = "DELETE from dbo.notes where NoteId=@id";
+
+        using (_sqlConnection) 
+        {
+            try
+            {
+                using var myCommand = GetCommand(query, _sqlConnection);
+                myCommand.Parameters.AddWithValue("@id", id);
+                    
+                if (_sqlConnection != null) _reader = GetReader(myCommand, _sqlConnection);
+                if (_reader != null) _table?.Load(_reader);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                _sqlConnection?.Close();
+            }
+        }
+
+        using (_table)
+        {
+            return new JsonResult(_table);
+        }
+    }
+        
+        
+        
+    [HttpDelete]
+    [Route("DeleteAllNotes")]
+    public JsonResult DeleteAllNotes()
+    {
+        const string query = "DELETE from dbo.notes";
+
+        using var myCon = _sqlConnection;
+        try
+        {
+            myCon?.Open();
+            using var myCommand = GetCommand(query, myCon);
+            myCommand.ExecuteNonQuery();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
             
-            using var command = new SqlCommand(query, myCon);
+        return new JsonResult("All notes have been deleted successfully.");
+    }
+        
+        
+
+    [HttpPut]
+    [Route("UpdateNote")]
+    public JsonResult UpdateNote([FromForm] int id, [FromForm] string title, [FromForm] string description)
+    {
+        const string query = "UPDATE dbo.notes SET Title = @title, Description = @description WHERE NoteId = @id";
+
+        using var myCon = GetConnection(_sqlDataSource);
+        using var command = GetCommand(query, myCon);
+        try
+        {
+            myCon.Open();
             command.Parameters.AddWithValue("@id", id);
             command.Parameters.AddWithValue("@title", title);
             command.Parameters.AddWithValue("@description", description);
-            
-            var rowsAffected = command.ExecuteNonQuery();
-
-            return rowsAffected > 0 ? new JsonResult("Updated " + title) 
-                : new JsonResult("Note not found or no changes made.");
         }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            throw;
+        }
+            
+        var rowsAffected = command.ExecuteNonQuery();
+
+        return rowsAffected > 0 ? new JsonResult("Updated " + title) 
+            : new JsonResult("Note not found or no changes made.");
     }
 }
